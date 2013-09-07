b0526d086866be761be482f694c4322e61913b0b
Todd Sifleet <todd.sifleet@gmail.com>
2013-09-07 13:04:21 -0400

    update test_sketch

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -9,26 +9,27 @@ byte motor_count = 3;
 //3 Motors on PORTA and 3 limit switches on PORTC
 MultiStepper stepper_controller(&PORTA, &DDRA, motor_count, &PINC, &DDRC, steps_per_rev);
 
-long step_count = 0;
-long start_of_rotation = 0;
 void setup() {
   stepper_controller.setPrinter(Serial);
-  stepper_controller.setSpeed(1);
+  stepper_controller.setSpeed(100);
   Serial.begin(9600);
 }
 
-long direction[] = {1, 1, 1};
-void loop() {
-      step_count++;
-      if (step_count % steps_per_rev == 0) {
-        Serial.print(millis() - start_of_rotation);
-        Serial.write("\n");
-         for (int i = 0; i < 3; i++) {
-           direction[i] *= -1;
-         }
-         start_of_rotation = millis();
+void osscilate() {
+  long direction[] = {1, 1, 1};
+  long step_count = 0;
+  while (true) {
+    step_count++;
+    if (step_count % steps_per_rev == 0) {
+      for (int i = 0; i < 3; i++) {
+        direction[i] *= -1;
       }
-        
-      stepper_controller.move(direction);
+    }
+    stepper_controller.move(direction);
+  }
+}
+
+void loop() {
+  osscilate();
 }
 
